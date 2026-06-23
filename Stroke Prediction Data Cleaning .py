d62b48e94c07d8bc22300c665d59e5b5beef141e
Shriya Sree Aachi <ssaachi1929@gmail.com>
2026-06-23 16:47:13 -0500

    Update Stroke Prediction Data Cleaning .py

--- Stroke Prediction Data Cleaning .py
+++ Stroke Prediction Data Cleaning .py
@@ -5,6 +5,7 @@ new_df = df.dropna()
 df_duplicates = new_df.drop_duplicates()
 df_filter = df_duplicates[df_duplicates['age'] >= 50.0]
 df_filter.to_csv('Updated healthcare-dataset-stroke-data.csv', index = False)
+#print("File was Created!")
 print(os.getcwd())
 print(df_filter['gender'].unique())
 print(df_filter['work_type'].unique())
